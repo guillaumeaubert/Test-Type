83a2be0eb3d78e302888ab141b389807bf53dbe3
Guillaume Aubert <aubertg@cpan.org>
2012-07-29 22:49:31 -0400

    Added skeleton for Test::Type.

---
+++ lib/Test/Type.pm
@@ -0,0 +1,100 @@
+package Test::Type;
+
+use strict;
+use warnings;
+
+use Carp qw();
+use Data::Validate::Type;
+use Exporter 'import';
+use Test::More qw();
+
+
+=head1 NAME
+
+Test::Type - Functions to validate data types in test files.
+
+
+=head1 VERSION
+
+Version 1.0.0
+
+=cut
+
+our $VERSION = '1.0.0';
+
+
+=head1 SYNOPSIS
+
+	use Test::Type;
+
+=cut
+
+our @EXPORT = qw(
+);
+
+
+=head1 FUNCTIONS
+
+
+=head1 AUTHOR
+
+Guillaume Aubert, C<< <aubertg at cpan.org> >>.
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-dist-versionsync at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=test-type>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+	perldoc Test::Type
+
+
+You can also look for information at:
+
+=over
+
+=item *
+
+RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=test-type>
+
+=item *
+
+AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/test-type>
+
+=item *
+
+CPAN Ratings
+
+L<http://cpanratings.perl.org/d/test-type>
+
+=item *
+
+Search CPAN
+
+L<http://search.cpan.org/dist/test-type/>
+
+=back
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012 Guillaume Aubert.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
